78e2c37a93fee07b9a2aa3092f67464b80332944
JuanRonchetti <jronchetti@frba.utn.edu.ar>
2023-07-03 18:10:55 -0300

    Trabajo Semanal 6

---
+++ TS_6/Sim.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 17:54:18 2023
+
+@author: jron0
+"""
+
+import sympy as sp
+import numpy as np
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+from pytc2.sistemas_lineales import analyze_sys, tf2sos_analog, pretty_print_SOS, pretty_print_lti
+
+
+num=[1/9, 0, 1]
+den= np.polymul([1, 1], [1, 1, 1])
+
+tf = sig.TransferFunction(num, den)
+analyze_sys( tf, 'LPF Prototipo' )
+
+num_hp, den_hp = sig.lp2hp(num, den)
+analyze_sys( sig.TransferFunction(num_hp, den_hp), 'HPF Objetivo' )
+
+
+
+
+
+
